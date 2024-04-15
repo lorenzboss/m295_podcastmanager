4b7fd6e773da66e4054d8dba6983bcdbe3e115c8
lorenz <lorenz.boss@bbzbl-it.ch>
2024-04-15 15:49:40 +0200

    changed the structure of the project

--- podcastmanager/src/main/java/ch/bossgroup/podcastmanager/artist/ArtistRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.bossgroup.podcastmanager.artist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
--- podcastmanager/src/main/java/ch/bossgroup/podcastmanager/podcast/PodcastRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.bossgroup.podcastmanager.podcast;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PodcastRepository extends JpaRepository<Podcast, Long> {
-}
