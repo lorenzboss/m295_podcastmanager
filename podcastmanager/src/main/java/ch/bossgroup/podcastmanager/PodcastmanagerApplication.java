7d1f514c126ddf52103d2cd293a593f7a7dcb630
lorenz <lorenz.boss@bbzbl-it.ch>
2024-04-15 13:48:18 +0200

    Initial commit

---
+++ podcastmanager/src/main/java/ch/bossgroup/podcastmanager/PodcastmanagerApplication.java
@@ -0,0 +1,13 @@
+package ch.bossgroup.podcastmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastmanagerApplication.class, args);
+	}
+
+}
---
+++ podcastmanager/src/test/java/ch/bossgroup/podcastmanager/PodcastmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bossgroup.podcastmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
