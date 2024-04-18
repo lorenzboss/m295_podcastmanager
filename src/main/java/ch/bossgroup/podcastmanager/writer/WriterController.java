46d1d840f74c1a88edb5b2058754ca1b3c78cd9d
lorenz <lorenz.boss@bbzbl-it.ch>
2024-04-18 16:19:12 +0200

    edit Writer and add WriterController and WriterService

---
+++ src/main/java/ch/bossgroup/podcastmanager/writer/WriterController.java
@@ -0,0 +1,57 @@
+package ch.bossgroup.podcastmanager.writer;
+
+import ch.bossgroup.podcastmanager.security.Roles;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@SecurityRequirement(name = "bearerAuth")
+@Validated
+public class WriterController {
+
+    private final WriterService writerService;
+
+
+    public WriterController(WriterService writerService) {
+        this.writerService = writerService;
+    }
+
+    @GetMapping("/api/writer/{id}")
+    @RolesAllowed(Roles.user)
+    public ResponseEntity<Writer> getWinner(@PathVariable Long id) {
+        return new ResponseEntity<>(writerService.getWriter(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/writer")
+    @RolesAllowed(Roles.user)
+    public ResponseEntity<List<Writer>> getAllWinner() {
+        return new ResponseEntity<>(writerService.getAllWriters(), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/writer")
+    @RolesAllowed(Roles.staff)
+    public ResponseEntity<Writer> createWinner(@RequestBody Writer writer) {
+        return new ResponseEntity<>(writerService.saveWriter(writer), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/api/writer/{id}")
+    @RolesAllowed(Roles.staff)
+    public ResponseEntity<Writer> editWinner(@PathVariable Long id, @RequestBody Writer writer) {
+        return new ResponseEntity<>(writerService.editWriter(writer, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/writer/{id}")
+    @RolesAllowed(Roles.admin)
+    public ResponseEntity<Writer> deleteWinner(@PathVariable Long id) {
+        writerService.deleteWriter(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/ch/bossgroup/podcastmanager/writer/WriterService.java
@@ -0,0 +1,45 @@
+package ch.bossgroup.podcastmanager.writer;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class WriterService {
+
+    private final WriterRepository repository;
+
+    public WriterService(WriterRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Writer> getAllWriters() {
+        return repository.findByOrderById();
+    }
+
+    public Writer getWriter(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Writer saveWriter(Writer writer) {
+        return repository.save(writer);
+    }
+
+    public Writer editWriter(Writer writer, Long id) {
+        if (repository.existsById(id)) {
+            Writer newWriter = new Writer(id, writer.getFirstname(), writer.getLastname());
+            return repository.save(newWriter);
+        } else {
+            throw new NoSuchElementException("Object with Id: " + id + " does not exist!");
+        }
+    }
+
+    public void deleteWriter(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new NoSuchElementException("Object with Id: " + id + " does not exist!");
+        }
+    }
+}
