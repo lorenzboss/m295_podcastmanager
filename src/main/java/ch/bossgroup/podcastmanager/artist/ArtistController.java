48d3a39ee939d940f78c74e0ac1b99ba30fbe17f
lorenz <lorenz.boss@bbzbl-it.ch>
2024-04-18 13:12:43 +0200

    add ArtistController and ArtistService methods

--- src/main/java/ch/bossgroup/podcastmanager/artist/ArtistController.java
+++ src/main/java/ch/bossgroup/podcastmanager/artist/ArtistController.java
@@ -1,16 +1,18 @@
 package ch.bossgroup.podcastmanager.artist;
 
-import ch.bossgroup.podcastmanager.podcast.Podcast;
+import ch.bossgroup.podcastmanager.security.Roles;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@SecurityRequirement(name = "bearerAuth")
+@Validated
 public class ArtistController {
 
     private final ArtistService artistService;
@@ -20,13 +22,36 @@ public class ArtistController {
         this.artistService = artistService;
     }
 
-    @GetMapping("/api/podcast")
-    public ResponseEntity<List<Artist>> getPodcasts() {
+    @GetMapping("/api/artist/{id}")
+    @RolesAllowed(Roles.user)
+    public ResponseEntity<Artist> getPodcast(@PathVariable Long id) {
+        return new ResponseEntity<>(artistService.getArtist(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/artist")
+    @RolesAllowed(Roles.user)
+    public ResponseEntity<List<Artist>> getAllPodcast() {
         return new ResponseEntity<>(artistService.getAllArtists(), HttpStatus.OK);
     }
 
-    @PostMapping("/api/podcast")
+    @PostMapping("/api/artist")
+    @RolesAllowed(Roles.staff)
     public ResponseEntity<Artist> createPodcast(@RequestBody Artist artist) {
-        return new ResponseEntity<>(artist, HttpStatus.CREATED);
+        return new ResponseEntity<>(artistService.saveArtist(artist), HttpStatus.CREATED);
     }
+
+    @PutMapping("/api/artist/{id}")
+    @RolesAllowed(Roles.staff)
+    public ResponseEntity<Artist> editPodcast(@PathVariable Long id, @RequestBody Artist artist) {
+        return new ResponseEntity<>(artistService.editArtist(artist, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/artist/{id}")
+    @RolesAllowed(Roles.admin)
+    public ResponseEntity<Artist> deletePodcast(@PathVariable Long id) {
+        artistService.deleteArtist(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/ch/bossgroup/podcastmanager/artist/ArtistService.java
+++ src/main/java/ch/bossgroup/podcastmanager/artist/ArtistService.java
@@ -3,6 +3,7 @@ package ch.bossgroup.podcastmanager.artist;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ArtistService {
@@ -14,7 +15,7 @@ public class ArtistService {
     }
 
     public List<Artist> getAllArtists() {
-        return repository.findAll();
+        return repository.findByOrderById();
     }
 
     public Artist getArtist(Long id) {
@@ -25,20 +26,20 @@ public class ArtistService {
         return repository.save(artist);
     }
 
-    public Artist updateArtist(Artist artist, Long id) {
-        return repository.findById(id)
-                .map(originalArtist -> {
-                    originalArtist.setId(id);
-                    originalArtist.setFirstname(artist.getFirstname());
-                    originalArtist.setLastname(artist.getLastname());
-                    return repository.save(originalArtist);
-                })
-                .orElse(null);
+    public Artist editArtist(Artist artist, Long id) {
+        if (repository.existsById(id)) {
+            Artist newArtist = new Artist(id, artist.getFirstname(), artist.getLastname());
+            return repository.save(newArtist);
+        } else {
+            throw new NoSuchElementException("Object with Id: " + id + " does not exist!");
+        }
     }
 
     public void deleteArtist(Long id) {
-        repository.deleteById(id);
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new NoSuchElementException("Object with Id: " + id + " does not exist!");
+        }
     }
-
-
 }
